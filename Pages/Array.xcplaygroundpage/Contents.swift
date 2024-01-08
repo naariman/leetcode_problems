680200eb80cb0cca2a3295ce19e5af0aecd0d488
Nariman Nogaibayev <pticafugu@gmail.com>
2024-01-09 00:56:28 +0600

    isPalindrome

--- Pages/Array.xcplaygroundpage/Contents.swift
+++ Pages/Array.xcplaygroundpage/Contents.swift
@@ -67,3 +67,5 @@ isAnagram(
     "anagram",
     "nagaram"
 )
+
+
--- Pages/String.xcplaygroundpage/Contents.swift
+++ Pages/String.xcplaygroundpage/Contents.swift
@@ -1,1 +1,7 @@
+func isPalindrome(_ x: Int) -> Bool {
+    var newArr = Array(String(x))
+    newArr.reverse()
+    return newArr == Array(String(x))
+}
 
+isPalindrome(10)
